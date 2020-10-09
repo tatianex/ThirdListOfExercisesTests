872c33c72138804c26b7f55e968028d7f10f6916
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-10-09 18:37:05 -0300

    added first structures for the test number one

---
+++ ExercisesTests.cs
@@ -0,0 +1,15 @@
+using System;
+{
+    namespace ThirdListOfExercisesTests
+    {
+        public class ThirdListOfExercisesTests
+        {
+            [Fact]
+            public void should_return_the_numbers_changed_from_array_a_to_array_b()
+            {
+                
+            }
+        {
+        }
+    }
+}
