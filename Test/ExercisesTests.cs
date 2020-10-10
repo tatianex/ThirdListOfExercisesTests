1c7a372f3db7c5293b8901031ad69b786fe1d51c
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-10-09 21:14:08 -0300

    working on exercise 1

--- Test/ExercisesTests.cs
+++ Test/ExercisesTests.cs
@@ -9,11 +9,13 @@ namespace ThirdListOfExercisesTests
         [Theory]
         [InlineData(new double[5]{0, 12, 15.4, 17, -2.9}, new double[5]{8.9, 94, -30, 18, 10})]
         [InlineData(new double[5]{2, -24.2, 45, -11, 4.8}, new double[5]{14, 22.2, -7, -20.5, -3})]
-        public void should_return_the_numbers_changed_from_array_a_to_array_b(int[] a, int[] b, int[] expected)
+        public void should_return_the_numbers_changed_from_array_a_to_array_b(double[] a, double[] b)
         {
-            var excercises = new ExercisesTests();
+            var exercises = new Exercises();
 
-            var result = Exercises.Exercise1(a, b);          
+            var result = exercises.Exercise1(a, b);
+
+            var expected = (new double[5]{8.9, 94, -30, 18, 10}, new double[5]{0, 12, 15.4, 17, -2.9});
 
             Assert.Equal(expected, result);
         }
